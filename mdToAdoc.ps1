8ad2517705bfc8bc1dac2f4454d61b2b29daa4bb
Konrad Jagusiak <konrad.jagusiak@dnv.com>
2025-05-10 23:28:55 +0200

    Add files via upload

---
+++ mdToAdoc.ps1
@@ -0,0 +1,18 @@
+﻿$sourceFolder = ""
+$targetFolder = ""
+
+if (-not (Test-Path -Path $targetFolder)) {
+    New-Item -ItemType Directory -Path $targetFolder | Out-Null
+}
+
+Get-ChildItem -Path $sourceFolder -Filter *.md -Recurse | ForEach-Object {
+    $relativePath = $_.FullName.Substring($sourceFolder.Length).TrimStart('\')
+    $outputPath = Join-Path $targetFolder ($relativePath -replace '\.md$', '.adoc')
+    $outputDir = Split-Path -Path $outputPath
+    if (-not (Test-Path -Path $outputDir)) {
+        New-Item -ItemType Directory -Path $outputDir -Force | Out-Null
+    }
+
+    pandoc $_.FullName -f markdown -t asciidoc -o $outputPath
+    Write-Host "Converted: $($_.FullName) → $outputPath"
+}
